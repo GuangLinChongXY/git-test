be7859ddd6dc8f7920d512ddc83b0e8483e858e3
xy1999 <xy1999@atguigu.com>
2022-01-26 15:12:57 +0800

    "first commit"

---
+++ src/main/java/com/atguigu/Demo01.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @description:
+ * @author: xiaoyi
+ * @time: 2022/1/26
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
